32248126b8fa958d1794e2fc6374f48987836e2e
scristiansc <scristian1012@gmail.com>
2024-10-18 15:36:03 -0500

    make a private attribute (products) in shopping cart

--- e-commerce/shopping_cart.py
+++ e-commerce/shopping_cart.py
@@ -1,29 +1,29 @@
 
 class ShoppingCart:
     def __init__(self):
-        self.products = []
+        self.__products = []
 
 
     def add_item(self, product):
-        if product not in self.products:
-            self.products.append(product)
+        if product not in self.__products:
+            self.__products.append(product)
         else:
             print("The product is already in the cart")
 
 
     def remove_item(self, product):
-        if product in self.products:
-            self.products.remove(product)
+        if product in self.__products:
+            self.__products.remove(product)
         else:
             print("The product is not in the cart")
 
 
     def get_items(self):
-        return self.products
+        return self.__products
 
 
     def get_total(self):
         total = 0
-        for product in self.products:
+        for product in self.__products:
             total += product.price
         return total
